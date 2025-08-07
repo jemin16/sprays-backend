29a6724d5e6ca69dd0ef38a0319d08b317d24ef7
Jemin Dodiya <jemindodiya61@gmail.com>
2025-08-07 11:15:39 +0530

    Create Server For Spray Backend

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
